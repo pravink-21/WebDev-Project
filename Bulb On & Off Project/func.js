905b52f6225ad51c785f172781837ae9086627aa
Pravin <91046560+pravink-21@users.noreply.github.com>
2022-09-06 18:21:45 +0530

    Add files via upload

---
+++ Bulb On & Off Project/func.js
@@ -0,0 +1,9 @@
+function myfun(n){
+    if(n==true){
+        document.getElementById('bulb').src="light-bulb-on.png"
+    }
+    else{
+        
+        document.getElementById('bulb').src="light-bulb-off.png"
+    }
+}
